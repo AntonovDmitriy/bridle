b49cff5159748a07d9989d7cec42b6508ea8c7ce
Dmitrii_Antonov <antonov.buscorr@gmail.com>
2023-06-21 00:48:35 +0200

    #32 actions

--- bridle-gateway/src/main/java/com/bridle/configuration/ComponentNameConstants.java
+++ bridle-gateway/src/main/java/com/bridle/configuration/ComponentNameConstants.java
@@ -9,7 +9,12 @@ public final class ComponentNameConstants {
     public static final String REST_CALL_COMPONENT_NAME = "rest-call";
 
     public static final String REST_IN_COMPONENT_NAME = "rest-in";
+
     public static final String SCHEDULER_COMPONENT_NAME = "main-scheduler";
 
     public static final String REST_POLL_COMPONENT_NAME = "rest-poll";
+
+    private ComponentNameConstants() {
+        throw new IllegalStateException("Utility class");
+    }
 }
