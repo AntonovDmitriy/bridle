da35a8dfc2e6904da8a1764805b82a051e9eb014
Dmitrii_Antonov <antonov.buscorr@gmail.com>
2023-07-24 18:34:41 +0200

    sf

--- bridle-gateway/src/test/java/composetest/KafkaHttpKafkaProcessingHeaderCollectorComposeTest.java
+++ bridle-gateway/src/test/java/composetest/KafkaHttpKafkaProcessingHeaderCollectorComposeTest.java
@@ -1,6 +1,7 @@
 package composetest;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.DockerComposeContainer;
@@ -9,16 +10,19 @@ import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 import org.testcontainers.junit.jupiter.Container;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.function.Predicate;
 
-import static com.bridle.configuration.routes.HttpKafkaConfiguration.GATEWAY_TYPE_HTTP_KAFKA;
+import static com.bridle.configuration.routes.KafkaHttpKafkaConfiguration.GATEWAY_TYPE_KAFKA_HTTP_KAFKA;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static utils.KafkaContainerUtils.countMessages;
 import static utils.MetricsTestUtils.parseMessagesAmount;
 import static utils.MetricsTestUtils.verifyMetrics;
 
-class HttpKafkaComposeTest {
+class KafkaHttpKafkaProcessingHeaderCollectorComposeTest {
 
-    private static final String ROUTE_NAME = GATEWAY_TYPE_HTTP_KAFKA;
+    private static final String ROUTE_NAME = GATEWAY_TYPE_KAFKA_HTTP_KAFKA;
 
     private static final Predicate<String> APP_STARTS_TO_RECIEVE_LOAD_PREDICATE =
             s -> parseMessagesAmount(s, ROUTE_NAME) > 0;
@@ -26,11 +30,14 @@ class HttpKafkaComposeTest {
     @Container
     private static final DockerComposeContainer<?> ENVIRONMENT = initEnvironment();
 
-    private static final String COMPOSE_FILE_PATH = "compose/demo-http-kafka-compose.yml";
+    private static final String COMPOSE_FILE_PATH =
+            "compose/demo-kafka-http-kafka-processing-headercollector-compose.yml";
 
     private static final String SERVICE_NAME_GATEWAY = "gateway";
 
     private static final int SERVICE_PORT = 8080;
+
+    private static final String ENDPOINT_NAME = "kafka";
 
     private static DockerComposeContainer initEnvironment() {
         return new DockerComposeContainer<>(new File(COMPOSE_FILE_PATH))
@@ -68,9 +75,22 @@ class HttpKafkaComposeTest {
                       result -> result == 0,
                       result -> result == 0,
                       buildPrometheusEndpoint(address, port));
+        assertTrue(countMessagesInKafka() > 0);
     }
 
     private String buildPrometheusEndpoint(String address, Integer port) {
         return String.format("http://%s:%d/actuator/prometheus", address, port);
+    }
+
+    private int countMessagesInKafka() {
+        return Assertions.assertDoesNotThrow(() -> {
+            try {
+                return countMessages(ENVIRONMENT.getContainerByServiceName(ENDPOINT_NAME).orElseThrow(),
+                                     "routetest_response",
+                                     "kafka:9092");
+            } catch (IOException | InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        });
     }
 }
