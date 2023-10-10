4c1f93eccb15b191c37f28a6ea540ef2f2513759
Dmitrii_Antonov <antonov.buscorr@gmail.com>
2023-10-10 16:30:46 +0200

    fw

--- bridle-gateway/src/test/resources/routetest/kafka-sql-kafka/function/in-and-out-params/init-oracle/start.sh
+++ bridle-gateway/src/test/resources/routetest/kafka-sql-kafka/function/in-and-out-params/init-oracle/start.sh
@@ -1,13 +1,15 @@
 sqlplus -s testUser/testPassword@//localhost/testDB <<EOF
 ALTER SESSION SET CURRENT_SCHEMA = TESTUSER;
 
-CREATE OR REPLACE FUNCTION add_numbers(
+CREATE OR REPLACE FUNCTION add_numbers_with_difference(
     p_number1 IN NUMBER,
-    p_number2 IN NUMBER
+    p_number2 IN NUMBER,
+    p_difference OUT NUMBER
 ) RETURN NUMBER IS
 BEGIN
+    p_difference := p_number1 - p_number2;
     RETURN p_number1 + p_number2;
-END add_numbers;
+END add_numbers_with_difference;
 /
 
 EOF
